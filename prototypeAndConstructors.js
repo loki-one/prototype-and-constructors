ff8cd87cd25646adee4ec0b6def647ef43fa7af1
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2017-11-01 22:46:21 +0100

    Add comments for understanding and code for prototype

--- prototypeAndConstructors.js
+++ prototypeAndConstructors.js
@@ -1,8 +1,21 @@
-function Dog(){
+function Dog(name){
   //this is set to an empty object {}
   //this is returned
+  this.name = name;
+  //To remove redundant fetch function and add it to Dog.prototype instead.
+  //this.fetch = function(){};
 }
 
-var testDog = new Dog();
+//prototype makes is easier to distinguish between shared and unique properties
+Dog.prototype.fetch = function(){
+  console.log('fetch inside of Dog.prototype');
+};
+// var testDog = new Dog('test dog');
+// console.log(testDog);
 
-console.log(testDog);
+var myDog = new Dog('Alexis');
+var randomDog = new Dog('Hey');
+
+console.log(Object.getPrototypeOf(myDog));
+console.log(Object.getPrototypeOf(myDog) === Dog.prototype);
+console.log(Object.getPrototypeOf(Dog.prototype));
